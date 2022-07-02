71c949227460ceb6564e15812e79b83a5d9d48a0
VighneshBilgi <vhbilgi@gmail.com>
2022-07-02 18:08:02 +0530

    Added: UC4_Switch_Case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
 
+
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_DAY_HOUR = 4;
@@ -12,16 +13,21 @@ public class EmployeeWage {
         int dailyWage =0;
 
         int attendance = (int)(Math.random()*10)%3;
-        if (attendance == 0){
-            System.out.println("Employee is absent");
-        } else if (attendance == 1) {
-            System.out.println("Employee is present Full Time");
-            dailyWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
-        } else {
-            System.out.println("Employee is present Part Time");
-            dailyWage = WAGE_PER_HOUR*PART_DAY_HOUR;
+
+        switch (attendance){
+            case 1 :
+                System.out.println("Employee is present Full Time");
+                dailyWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
+                break;
+            case 2 :
+                System.out.println("Employee is present Part Time");
+                dailyWage = WAGE_PER_HOUR*PART_DAY_HOUR;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
 
         System.out.println("Daily Wage :"+dailyWage);
+
     }
 }
